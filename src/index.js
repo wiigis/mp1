db46d1971724f8cf5bf227e563378b54b5bc5aa9
wiigs <rappi3@illinois.edu>
2026-09-20 00:28:23 -0500

    lots of progress. on modals

--- src/index.js
+++ src/index.js
@@ -11,3 +11,85 @@ import './css/main.scss';
 
 // Scripts
 import './js/main.js';
+
+const navbar = document.querySelector("nav");
+const sections = document.querySelectorAll("section"); 
+const navLinks = document.querySelectorAll(".nav-links a"); 
+
+// detect scrolling, shrink navbar when detected
+window.addEventListener("scroll", function() {
+
+    if (window.scrollY > 0) {
+        navbar.classList.add("small");
+    } else {
+        navbar.classList.remove("small");
+    }
+
+    let currentSection = "";
+
+    const navbarBottom = navbar.getBoundingClientRect().bottom;  // getting location of the bottom of navbar
+
+
+    sections.forEach(function (section) {
+
+        const sectionPosition = section.getBoundingClientRect();  // getting location of each section
+
+        // checking if section reached/passed bottom of navbar
+        // if so, then that is the section we are currently reading
+        if (sectionPosition.top <= navbarBottom) {
+            currentSection = section.id;
+        }
+
+    });
+
+    // this is special case user reached bottom of page
+    if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 1) {
+        currentSection = sections[sections.length - 1].id;
+    }
+
+    // updating navbar indicator
+    navLinks.forEach(function (link) {
+
+        link.classList.remove("active");  // removing active class from section 
+
+        // checking if current traversed section is equal to computed currentsection,
+        // if so we make that section apart of active class
+        if (link.getAttribute("href") === "#" + currentSection) { 
+            link.classList.add("active");
+        }
+
+    });
+
+});
+
+// interacting with modal (opening modal)
+const cards = document.querySelectorAll(".game-mode-card");
+
+cards.forEach(function(card) {
+
+    card.addEventListener("click", function() {
+
+        const modalId = card.getAttribute("data-modal");
+
+        const modal = document.getElementById(modalId);
+
+        modal.style.display = "flex";
+
+    });
+
+});
+
+// interacting with modal (closing modal)
+const closeButtons = document.querySelectorAll(".close");
+
+closeButtons.forEach(function(button) {
+
+    button.addEventListener("click", function() {
+
+        const modal = button.closest(".modal");
+
+        modal.style.display = "none";
+
+    });
+
+});
